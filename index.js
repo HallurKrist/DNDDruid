a2fe963845cbe97e06d4471306aa5b86a010141b
Hallur Kristinn Hallsson <hkh32@hi.is>
2020-01-18 21:31:16 +0000

    json modifaications

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
  const page = document.querySelector('body');
  const isLecturePage = page.classList.contains('lecture-page');
 
- fetch('https://drive.google.com/file/d/18tTF4zNNddHe-tyBOb_Q25DtjnTni02P/edit')
+ fetch('https://raw.githubusercontent.com/HallurKrist/DNDDruid/master/Wild%20Shape%20Cards%20(Legal).json')
    .then((result) => {
      if (!result.ok) {
        throw new Error('Non 200 status');
