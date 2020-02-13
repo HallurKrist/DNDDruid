c29cd1e4edc92386779ae18f2ec644679b5955fe
Hallur Kristinn Hallsson <hkh32@hi.is>
2020-02-13 18:04:10 +0000

    filter komið fyrir size og cr

--- src/lib/filter.js
+++ src/lib/filter.js
@@ -1,8 +1,10 @@
-
+import { empty } from './helpers';
+import { makeCards } from './card';
 
 var toBeDeleted = [];
 const data = JSON.parse(window.localStorage.getItem('DNDDruidBeasts'));
 var filterData;
+var dataDoneFiltering = [];
 
 export function filterCards() {
   filterData = JSON.parse(window.localStorage.getItem('filter'));
@@ -11,41 +13,57 @@ export function filterCards() {
   console.log('to be filtered Size: ' + filterData.Size);
   console.log('to be filtered Speed: ' + filterData.Speed);
 
-  
+  toBeDeleted = [];
+  dataDoneFiltering = [];
 
   filterCR();
   filterSize();
 
   compressDeleted();
+  deleteFiltered();
 
-  
+  const cards = document.querySelector('.cards');
+  empty(cards);
+  const thePage = cards.parentElement;
+  thePage.removeChild(cards);
+
+  if (dataDoneFiltering.length === 0) {
+    makeCards(thePage, data);
+  } else {
+    makeCards(thePage, dataDoneFiltering);
+  }
 }
 
 function filterCR() {
-  var j;
-  for (j = 0; j < data.length; j++) {
-    var thedata = data[j];
-    if (!filterData.CR.includes(`${thedata.CR}-CR`)) {
-      toBeDeleted.push(j);
-      debugger;
+  if (filterData.CR && filterData.CR.length !== 0) {
+    var j;
+    for (j = 0; j < data.length; j++) {
+      var thedata = data[j];
+      if (!filterData.CR.includes(`${thedata.CR}-CR`)) {
+        toBeDeleted.push(j);
+        // debugger;
+      }
     }
+    console.log('after cr filter ');
+    console.log(toBeDeleted);
   }
-  console.log('after cr filter ');
-  console.log(toBeDeleted);
 }
 
 function filterSize() {
-  var j;
-  for (j = 0; j < data.length; j++) {
-    var beastSize = data[j].contents.size;
-    var sizeOfBeast = beastSize.split(" ");
+  if (filterData.Size && filterData.Size.length !== 0) {
+    var j;
+    for (j = 0; j < data.length; j++) {
+      var beastSize = data[j].contents.size;
+      var sizeOfBeast = beastSize.split(" ");
 
-    if (!filterData.Size.includes(`${sizeOfBeast[0]}`)) {
-      toBeDeleted.push(j);
+      if (!filterData.Size.includes(`${sizeOfBeast[0]}`)) {
+        toBeDeleted.push(j);
+        // debugger;
+      }
     }
+    console.log('after size filter ');
+    console.log(toBeDeleted);
   }
-  console.log('after size filter ');
-  console.log(toBeDeleted);
 }
 
 
@@ -56,4 +74,17 @@ function compressDeleted() {
 
   console.log('after compressing filter ');
   console.log(toBeDeleted);
+}
+
+function deleteFiltered() {
+  var i;
+  for(i = 0; i < data.length; i++) {
+    if (toBeDeleted.includes(i)){
+      // debugger;
+    } else {
+      dataDoneFiltering.push(data[i]);
+    }
+  }
+  console.log('after deleting filtering');
+  console.log(dataDoneFiltering);
 }
